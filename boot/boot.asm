f7f96f4f773688b0dbd675a014f683626bcdf61c
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-14 17:55:12 +0100

    Update boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,21 +1,35 @@
 ; boot.asm
-; 32-bit Multiboot header
 
 section .multiboot
 align 4
 
-dd 0x1BADB002
-dd 0x0
-dd -(0x1BADB002)
+MBALIGN  equ 1 << 0
+MEMINFO  equ 1 << 1
+FLAGS    equ MBALIGN | MEMINFO
+MAGIC    equ 0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS)
+
+dd MAGIC
+dd FLAGS
+dd CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+resb 16384          ; 16 KB stack
+stack_top:
 
 section .text
-global _start
+global start
 extern kernel_main
 
-_start:
+start:
+    cli
+
+    mov esp, stack_top
+
     call kernel_main
 
-hang:
-    cli
+.hang:
     hlt
-    jmp hang
+    jmp .hang
