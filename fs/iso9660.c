cb9055fbd2e4f7e21b1e32b6a2960687d6e0742a
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-18 12:17:44 +0100

    Update iso9660.c

--- fs/iso9660.c
+++ fs/iso9660.c
@@ -1,1 +1,54 @@
+#include "iso9660.h"
 
+#include "../drivers/cdrom.h"
+
+
+
+extern void print(char *text);
+
+
+
+/*
+    ISO9660 is initialized here
+*/
+
+void iso_init(void)
+{
+    print("ISO9660 initialized\n");
+}
+
+
+
+/*
+    Read file from ISO
+
+    Temporary version.
+
+    Next step:
+    - Read Primary Volume Descriptor
+    - Read directory records
+    - Locate file
+    - Read sectors
+*/
+
+bool iso_read_file(
+    char *name,
+    uint8_t *buffer,
+    uint32_t *size
+)
+{
+
+    print("Reading ISO file: ");
+
+    print(name);
+
+    print("\n");
+
+
+    /*
+        Not implemented yet
+    */
+
+
+    return false;
+}
