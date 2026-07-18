8442a1d9701e1fb8db6661dab43fc11a2c279227
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-18 12:49:42 +0100

    Update grub_install.c

--- installer/grub_install.c
+++ installer/grub_install.c
@@ -1,1 +1,59 @@
+#include "grub_install.h"
 
+#include "../drivers/disk.h"
+
+
+extern void print(char *text);
+
+
+
+/*
+    Temporary GRUB installer
+
+    This currently checks that the
+    disk can be written.
+
+    Real GRUB core image writing
+    will be added after we add
+    ISO file reading.
+*/
+
+
+bool install_grub(void)
+{
+    uint8_t buffer[512];
+
+
+    print("Installing GRUB...\n");
+
+
+    /*
+        Test writing sector 1.
+
+        Later this will contain:
+        - boot.img
+        - core.img
+    */
+
+
+    for(int i = 0; i < 512; i++)
+    {
+        buffer[i] = 0;
+    }
+
+
+
+    if(!disk_write(1, buffer))
+    {
+        print("GRUB install failed\n");
+
+        return false;
+    }
+
+
+
+    print("GRUB stage written\n");
+
+
+    return true;
+}
