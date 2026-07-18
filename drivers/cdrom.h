eb09cdbc747116694998cf0887e4a15adcadcf57
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-18 12:59:13 +0100

    Update cdrom.h

--- drivers/cdrom.h
+++ drivers/cdrom.h
@@ -3,13 +3,17 @@
 
 #include "../include/types.h"
 
+
 #define CD_SECTOR_SIZE 2048
 
+
 void cdrom_init(void);
+
 
 bool cdrom_read_sector(
     uint32_t sector,
     uint8_t *buffer
 );
+
 
 #endif
