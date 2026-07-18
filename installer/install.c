579ad6ce1f37fc868e2880450de89897f303a585
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-18 12:53:01 +0100

    Update install.c

--- installer/install.c
+++ installer/install.c
@@ -1,7 +1,8 @@
 #include "install.h"
 
-#include "../fs/filesystem.h"
 #include "grub_install.h"
+
+#include "../fs/filesystem.h"
 
 
 extern void print(char *text);
@@ -13,6 +14,11 @@ bool install_system(void)
     print("\nInstalling CustOS...\n\n");
 
 
+
+    /*
+        Format target drive
+    */
+
     print("Formatting drive...\n");
 
 
@@ -20,36 +26,67 @@ bool install_system(void)
 
 
 
+    /*
+        Install bootloader
+    */
+
+    if(!install_grub())
+    {
+        print("Bootloader installation failed\n");
+
+        return false;
+    }
+
+
+
+    /*
+        Create system files
+    */
+
     print("Creating system files...\n");
+
 
 
     if(!fs_create("KERNEL.BIN"))
     {
         print("Failed creating kernel file\n");
+
         return false;
     }
+
 
 
     if(!fs_create("SYSTEM.BIN"))
     {
         print("Failed creating system file\n");
+
         return false;
     }
+
 
 
     if(!fs_create("CONFIG.TXT"))
     {
         print("Failed creating config file\n");
+
         return false;
     }
 
 
 
+    /*
+        Save filesystem
+    */
+
     fs_sync();
 
 
 
-    print("\nInstallation complete!\n");
+    print("\nCustOS installation complete!\n");
+
+
+    print("You may restart the computer.\n");
+
 
 
     return true;
