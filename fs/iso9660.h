576a8a5805656fb984fc4ceee2e12a8d0b7f76f3
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-18 12:17:18 +0100

    Update iso9660.h

--- fs/iso9660.h
+++ fs/iso9660.h
@@ -1,1 +1,20 @@
+#ifndef ISO9660_H
+#define ISO9660_H
 
+#include "../include/types.h"
+
+
+#define ISO_SECTOR_SIZE 2048
+
+
+void iso_init(void);
+
+
+bool iso_read_file(
+    char *name,
+    uint8_t *buffer,
+    uint32_t *size
+);
+
+
+#endif
