fa8e74e3e59ad0c73987e6065abb5b1bfaf1928b
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-18 12:26:43 +0100

    Update installer.c

--- installer/installer.c
+++ installer/installer.c
@@ -1,1 +1,48 @@
+#include "installer.h"
 
+#include "install.h"
+
+
+extern void clear(void);
+extern void print(char *text);
+
+
+
+static void delay()
+{
+    for(volatile int i = 0; i < 5000000; i++)
+    {
+
+    }
+}
+
+
+
+void installer_start(void)
+{
+
+    clear();
+
+
+    print("Preparing for first CustOS boot...\n");
+
+
+    delay();
+
+
+
+    clear();
+
+
+    print("CustOS Installer\n");
+    print("================\n\n");
+
+
+    print("Starting installation...\n");
+
+
+    install_system();
+
+
+    print("\nYou can now restart your computer.\n");
+}
