26fd4fa7118678ec54eff311fe17bf7df5566a60
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-20 15:09:45 +0100

    tribute to our fallen soldiers - Roshan

--- fs/filesystem.h
+++ fs/filesystem.h
@@ -3,51 +3,49 @@
 
 #include "../include/types.h"
 
-/*
-    Custos Filesystem v1
-*/
 
-#define FS_MAGIC 0x43555354
+#define FS_MAGIC 0x43555354   /* "CUST" */
 
 #define FS_SECTOR_SIZE 512
 
-/* Disk layout */
 
 #define FS_SUPERBLOCK_SECTOR 1
+
 #define FS_FILE_TABLE_START 2
 
-/* Limits */
+#define FS_DATA_START 10
 
-#define FS_MAX_FILES 128
+
+
+#define FS_MAX_FILES 64
+
 #define FS_FILENAME_LENGTH 32
-#define FS_MAX_FILE_SIZE 4096
 
-/*
-    File entry flags
-*/
+#define FS_MAX_FILE_SIZE 65536
+
+
 
 #define FILE_UNUSED 0
 #define FILE_USED   1
 
-/*
-    Number of sectors needed for the file table.
 
-    This is calculated automatically so it always
-    matches the size of FileEntry.
-*/
 
 typedef struct
 {
     uint32_t magic;
+
     uint32_t total_sectors;
+
     uint32_t file_table_start;
+
     uint32_t data_start;
+
     uint32_t free_blocks;
+
 } SuperBlock;
 
-/*
-    One file entry
-*/
+
+
 
 typedef struct
 {
@@ -59,59 +57,89 @@ typedef struct
 
     uint8_t used;
 
-    /* keeps structure aligned */
-    uint8_t reserved[3];
-
 } FileEntry;
 
-#define FS_FILE_TABLE_SECTORS \
-((sizeof(FileEntry) * FS_MAX_FILES + FS_SECTOR_SIZE - 1) / FS_SECTOR_SIZE)
 
-#define FS_DATA_START \
-(FS_FILE_TABLE_START + FS_FILE_TABLE_SECTORS)
 
-/*
-    Filesystem
-*/
 
 typedef struct
 {
-    bool mounted;
-
     SuperBlock super;
 
     FileEntry files[FS_MAX_FILES];
 
+    bool mounted;
+
 } FileSystem;
 
-/* Global filesystem */
 
-extern FileSystem fs;
 
-/* Core */
+/*
+    Filesystem functions
+*/
+
 
 void fs_init(void);
-void fs_format(void);
+
+
 bool fs_mount(void);
+
+
+/*
+    Changed from void to bool
+
+    Returns false if disk writes fail
+*/
+
+bool fs_format(void);
+
+
+
 void fs_sync(void);
 
-/* File operations */
 
-int fs_find(char *name);
 
-bool fs_create(char *name);
+bool fs_create(
+    char *name
+);
 
-bool fs_delete(char *name);
 
-bool fs_write(char *name,
-              char *data,
-              uint32_t size);
 
-bool fs_read(char *name,
-             char *buffer);
+bool fs_write(
+    char *name,
+    char *data,
+    uint32_t size
+);
 
-uint32_t fs_size(char *name);
+
+
+bool fs_read(
+    char *name,
+    char *buffer
+);
+
+
+
+bool fs_delete(
+    char *name
+);
+
+
+
+int fs_find(
+    char *name
+);
+
+
+
+uint32_t fs_size(
+    char *name
+);
+
+
 
 void fs_list(void);
+
+
 
 #endif
