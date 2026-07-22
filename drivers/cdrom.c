b0ba96af094ff156fe0f7a5af594ba9b92879938
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-22 15:41:41 +0100

    Update cdrom.c

--- drivers/cdrom.c
+++ drivers/cdrom.c
@@ -35,7 +35,7 @@ extern void print(char *text);
 #define ATA_IDENTIFY        0xEC
 #define ATA_PACKET          0xA0
 
-#define ATAPI_READ12        0xA8
+#define ATAPI_READ10        0x28
 
 
 
@@ -76,10 +76,12 @@ static bool wait_drq(uint16_t io)
     {
         uint8_t status = inb(io + ATA_STATUS);
 
+
         if(status & 0x01)
         {
             return false;
         }
+
 
         if(status & 0x08)
         {
@@ -120,9 +122,8 @@ static uint8_t ide_identify(
 
 
     if(status == 0)
-    {
         return 0;
-    }
+
 
 
     while(status & 0x80)
@@ -131,14 +132,17 @@ static uint8_t ide_identify(
     }
 
 
+
     lba_mid = inb(io + ATA_LBA_MID);
     lba_high = inb(io + ATA_LBA_HIGH);
+
 
 
     if(lba_mid == 0x14 && lba_high == 0xEB)
     {
         return 2;
     }
+
 
 
     if(status & 0x01)
@@ -158,26 +162,20 @@ static void check_device(
     char *name
 )
 {
-    uint8_t result;
-
-    result = ide_identify(io, drive);
+    uint8_t result = ide_identify(io, drive);
 
 
     print(name);
 
 
     if(result == 0)
-    {
         print(": Empty\n");
-    }
+
     else if(result == 1)
-    {
         print(": ATA Hard Disk\n");
-    }
+
     else if(result == 2)
-    {
         print(": ATAPI CD-ROM\n");
-    }
 }
 
 
@@ -187,14 +185,16 @@ void cdrom_init(void)
     print("IDE DEVICE IDENTIFY\n");
 
 
-    check_device(PRIMARY_IO, 0, "Primary Master");
-    check_device(PRIMARY_IO, 1, "Primary Slave");
-    check_device(SECONDARY_IO, 0, "Secondary Master");
-    check_device(SECONDARY_IO, 1, "Secondary Slave");
+    check_device(PRIMARY_IO,0,"Primary Master");
+    check_device(PRIMARY_IO,1,"Primary Slave");
+    check_device(SECONDARY_IO,0,"Secondary Master");
+    check_device(SECONDARY_IO,1,"Secondary Slave");
 
 
     print("IDENTIFY COMPLETE\n");
 }
+
+
 
 
 
@@ -211,14 +211,10 @@ bool cdrom_read_sector(
     print("ATAPI: START\n");
 
 
-    // Select Secondary Master CD-ROM
-
-    outb(io + ATA_DEVICE, 0xA0);
+    outb(io + ATA_DEVICE,0xA0);
 
     ide_delay();
 
-
-    print("ATAPI: SELECTED\n");
 
 
     if(!wait_not_busy(io))
@@ -228,23 +224,25 @@ bool cdrom_read_sector(
     }
 
 
+
     print("ATAPI: READY\n");
 
 
-    // Set transfer size: 2048 bytes
 
-    outb(io + ATA_FEATURES, 0);
+    // 2048 byte transfer
 
-    outb(io + ATA_BYTE_COUNT_LOW, 0x00);
-    outb(io + ATA_BYTE_COUNT_HIGH, 0x08);
+    outb(io + ATA_FEATURES,0);
+
+    outb(io + ATA_BYTE_COUNT_LOW,0x00);
+    outb(io + ATA_BYTE_COUNT_HIGH,0x08);
 
 
-    // Send PACKET command
 
-    outb(io + ATA_COMMAND, ATA_PACKET);
+    outb(io + ATA_COMMAND,ATA_PACKET);
 
 
     print("ATAPI: PACKET SENT\n");
+
 
 
     if(!wait_drq(io))
@@ -254,19 +252,23 @@ bool cdrom_read_sector(
     }
 
 
+
     print("ATAPI: DRQ OK\n");
 
 
-    // Build READ(12) packet
 
-    for(int i = 0; i < 12; i++)
-    {
-        packet[i] = 0;
-    }
+    for(int i=0;i<12;i++)
+        packet[i]=0;
 
 
-    packet[0] = ATAPI_READ12;
 
+    // READ(10)
+
+    packet[0] = ATAPI_READ10;
+
+
+
+    // LBA
 
     packet[2] = (sector >> 24) & 0xFF;
     packet[3] = (sector >> 16) & 0xFF;
@@ -274,10 +276,13 @@ bool cdrom_read_sector(
     packet[5] = sector & 0xFF;
 
 
-    packet[9] = 1;
+
+    // Transfer 1 sector
+
+    packet[7] = 0;
+    packet[8] = 1;
 
 
-    // Send packet
 
     outsw(
         io + ATA_DATA,
@@ -289,51 +294,29 @@ bool cdrom_read_sector(
     print("ATAPI: READ COMMAND SENT\n");
 
 
-uint8_t status;
-int timeout = 100000;
 
-
-while(timeout--)
-{
-    status = inb(io + ATA_STATUS);
-
-
-    if(status & 0x01)
+    if(!wait_drq(io))
     {
         print("ATAPI: READ ERROR\n");
         return false;
     }
 
 
-    if(status & 0x08)
-    {
-        print("ATAPI: DATA READY\n");
-        break;
-    }
-}
 
-
-if(timeout == 0)
-{
-    print("ATAPI: STATUS TIMEOUT\n");
-    return false;
-}
+    print("ATAPI: READING DATA\n");
 
 
 
-
-print("ATAPI: READING DATA\n");
-
-
-insw(
-    io + ATA_DATA,
-    buffer,
-    CD_SECTOR_SIZE / 2
-);
+    insw(
+        io + ATA_DATA,
+        buffer,
+        CD_SECTOR_SIZE / 2
+    );
 
 
-print("ATAPI: READ OK\n");
+
+    print("ATAPI: READ OK\n");
 
 
-return true;
+    return true;
 }
