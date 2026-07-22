ad0b1759f83e6e454f254fc8232db8eb40766bc9
Isaac123800 <isaacpolomski1@gmail.com>
2026-07-22 15:05:19 +0100

    Update cdrom.c

--- drivers/cdrom.c
+++ drivers/cdrom.c
@@ -3,17 +3,10 @@
 #include "../include/types.h"
 #include "io.h"
 
-
 extern void print(char *text);
-
-
-// IDE ports
 
 #define PRIMARY_IO      0x1F0
 #define SECONDARY_IO    0x170
-
-
-// ATA registers
 
 #define ATA_DATA        0
 #define ATA_ERROR       1
@@ -25,169 +18,128 @@ extern void print(char *text);
 #define ATA_STATUS      7
 #define ATA_COMMAND     7
 
+#define ATA_IDENTIFY        0xEC
+#define ATA_PACKET          0xA0
 
-// Commands
-
-#define ATA_IDENTIFY            0xEC
-#define ATA_IDENTIFY_PACKET     0xA1
-
-
-
-static void ide_delay()
+static void ide_delay(void)
 {
-    inb(0x80);
-    inb(0x80);
-    inb(0x80);
-    inb(0x80);
+    io_wait();
+    io_wait();
+    io_wait();
+    io_wait();
 }
 
-
-
-static uint8_t ide_identify(
-    uint16_t io,
-    uint8_t drive
-)
+static uint8_t ide_identify(uint16_t io, uint8_t drive)
 {
     uint8_t status;
-    uint8_t lba_mid;
-    uint8_t lba_high;
-
-
-    // Select drive
+    uint8_t mid;
+    uint8_t high;
 
     outb(io + ATA_DEVICE, 0xA0 | (drive << 4));
 
     ide_delay();
-
-
-    // Clear registers
 
     outb(io + ATA_SECTOR, 0);
     outb(io + ATA_LBA_LOW, 0);
     outb(io + ATA_LBA_MID, 0);
     outb(io + ATA_LBA_HIGH, 0);
 
-
-    // Try ATA identify
-
     outb(io + ATA_COMMAND, ATA_IDENTIFY);
 
-
     status = inb(io + ATA_STATUS);
-
 
     if(status == 0)
     {
         return 0;
     }
 
-
-    // Wait for busy to clear
-
     while(status & 0x80)
     {
         status = inb(io + ATA_STATUS);
     }
 
+    mid = inb(io + ATA_LBA_MID);
+    high = inb(io + ATA_LBA_HIGH);
 
-    // Read LBA values to detect ATAPI
-
-    lba_mid = inb(io + ATA_LBA_MID);
-    lba_high = inb(io + ATA_LBA_HIGH);
-
-
-    /*
-        ATAPI signature:
-
-        LBA mid  = 0x14
-        LBA high = 0xEB
-    */
-
-    if(lba_mid == 0x14 && lba_high == 0xEB)
+    if(mid == 0x14 && high == 0xEB)
     {
-        return 2; // ATAPI device
+        return 2; /* ATAPI */
     }
-
 
     if(status & 0x01)
     {
         return 0;
     }
 
-
-    return 1; // ATA device
+    return 1; /* ATA */
 }
 
-
-
-static void check_device(
-    uint16_t io,
-    uint8_t drive,
-    char *name
-)
+static void print_device(uint16_t io, uint8_t drive, char *name)
 {
-    uint8_t result;
-
-
-    result = ide_identify(io, drive);
-
+    uint8_t type = ide_identify(io, drive);
 
     print(name);
 
-
-    if(result == 0)
+    if(type == 0)
     {
         print(": Empty\n");
     }
-    else if(result == 1)
+    else if(type == 1)
     {
         print(": ATA Hard Disk\n");
     }
-    else if(result == 2)
+    else
     {
         print(": ATAPI CD-ROM\n");
     }
 }
 
+static bool atapi_packet_test(void)
+{
+    uint8_t status;
 
+    /* Secondary Master = CD-ROM */
+    outb(SECONDARY_IO + ATA_DEVICE, 0xA0);
+
+    ide_delay();
+
+    outb(SECONDARY_IO + ATA_COMMAND, ATA_PACKET);
+
+    status = inb(SECONDARY_IO + ATA_STATUS);
+
+    if(status == 0)
+    {
+        return false;
+    }
+
+    while(status & 0x80)
+    {
+        status = inb(SECONDARY_IO + ATA_STATUS);
+    }
+
+    return true;
+}
 
 void cdrom_init(void)
 {
     print("IDE DEVICE IDENTIFY\n");
 
-
-    check_device(
-        PRIMARY_IO,
-        0,
-        "Primary Master"
-    );
-
-
-    check_device(
-        PRIMARY_IO,
-        1,
-        "Primary Slave"
-    );
-
-
-    check_device(
-        SECONDARY_IO,
-        0,
-        "Secondary Master"
-    );
-
-
-    check_device(
-        SECONDARY_IO,
-        1,
-        "Secondary Slave"
-    );
-
+    print_device(PRIMARY_IO, 0, "Primary Master");
+    print_device(PRIMARY_IO, 1, "Primary Slave");
+    print_device(SECONDARY_IO, 0, "Secondary Master");
+    print_device(SECONDARY_IO, 1, "Secondary Slave");
 
     print("IDENTIFY COMPLETE\n");
+
+    if(atapi_packet_test())
+    {
+        print("ATAPI PACKET OK\n");
+    }
+    else
+    {
+        print("ATAPI PACKET FAILED\n");
+    }
 }
-
-
 
 bool cdrom_read_sector(
     uint32_t sector,
